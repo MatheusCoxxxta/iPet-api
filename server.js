db142eb3f2ca8ef5b889fbfb365091da3aabf576
Matheus Costa <matheus271200.ms@gmail.com>
2020-05-13 08:04:06 -0300

    Deploy changes

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ require('dotenv').config()
 
 const app = express()
 const routes = require('./src/Routes')
+const cors = require('cors');
+
+
+app.use(cors());
+
 
 mongoose.connect("mongodb+srv://iPet:iPet1234@ipet-s4u0w.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
@@ -13,6 +18,6 @@ mongoose.connect("mongodb+srv://iPet:iPet1234@ipet-s4u0w.mongodb.net/test?retryW
 
 app.use('/api', routes)
 
-app.listen(3001)
+app.listen(process.env.PORT || 3001)
 
 // Server iPet
